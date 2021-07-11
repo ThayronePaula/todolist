b9458d28f5090f9573892172eb80c0e1304ce56f
thayrone <thayronedepaula15@gmail.com>
2021-07-11 13:31:03 -0300

    config dark mode

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "dark",
+
+  colors: {
+    text:"#fff",
+
+    background: "#000",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "light",
+
+  colors: {
+    text:"#000",
+    background: "#eee",
+
+  },
+};
